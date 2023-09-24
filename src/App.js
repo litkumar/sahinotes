36ed15e4340c221ae22595c765958e306ec05f43
Kumarsatwik <kumarsatwik6@gmail.com>
2023-09-24 19:44:57 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import './App.css';
 import Home from './FirstPage/Home';
 import Home2 from './SecondPage/Home2';
-<<<<<<< HEAD
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 function App() {
@@ -15,24 +13,6 @@ function App() {
       </Routes>
       </Router>
     </>
-=======
-=======
-import logo from './logo.svg';
-import './App.css';
-import Home from './FirstPage/Home';
-import Home2 from './SecondPage/Home2';
->>>>>>> e2a664c8fa4a51071f40ac092e4831600a1d2b28
-
-function App() {
-  return (
-    <div className="App">
-     {/* <Home/> */}
-     <Home2/>
-    </div>
-<<<<<<< HEAD
->>>>>>> e2a664c (First Commit)
-=======
->>>>>>> e2a664c8fa4a51071f40ac092e4831600a1d2b28
   );
 }
 
